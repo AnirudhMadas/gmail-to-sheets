28e3a27f6b5ac6f723f62b2875a20fc19355fadb
Anirudh <madasanirudh0@gmail.com>
2026-01-14 15:02:42 +0530

    added sheet id

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ CREDENTIALS_FILE = "credentials/credentials.json"
 TOKEN_FILE = "token.json"
 
 # ✅ Put your Google Sheet ID here
-SPREADSHEET_ID = "PASTE_YOUR_SPREADSHEET_ID_HERE"
+SPREADSHEET_ID = "1qV2ZUCQVza70zFvvk7t7vfrXysaRUWQnDcYrDk0nPmk"
 
 # Sheet range where data will be appended
 SHEET_RANGE = "Sheet1!A:E"
